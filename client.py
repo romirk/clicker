816e2552027a2329df6841a4f06a477d6f4f9b93
romir kulshrestha <romir.kulshrestha@gmail.com>
2023-11-14 07:05:49 +0100

    args

--- client.py
+++ client.py
@@ -1,16 +1,18 @@
+import argparse
+
 from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PublicKey
 
 from clicker import SusClient
 from clicker.util import logger_config
 
 
-def main():
+def main(host: str):
     logger_config()
     with open("server.pub", "r") as f:
         ppks_bytes = bytes.fromhex(key := f.read())
         print(f"Using public key {key}")
     ppks = X25519PublicKey.from_public_bytes(ppks_bytes)
-    client = SusClient("localhost", 42069, ppks, b"cliq")
+    client = SusClient(host, 42069, ppks, b"cliq")
     client.connection_made(True)
     client.send(b"hello world")
     client.send(b"goodbye world")
@@ -19,4 +21,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Clicker client")
+    parser.add_argument("host", type=str, default="localhost", help="host")
+    args = parser.parse_args()
+    main(args.host)
